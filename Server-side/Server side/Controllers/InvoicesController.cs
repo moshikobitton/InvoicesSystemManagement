22b59f83eb7d0b86d5daa97a6f26007e96ad8f79
moshikobitton <m6m66b@gmail.com>
2023-07-01 16:16:51 +0300

    create project

---
+++ Server-side/Server side/Controllers/InvoicesController.cs
@@ -0,0 +1,75 @@
+﻿using AirBNB.Models;
+using Server_side.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Server_side.Controllers
+{
+    public class InvoicesController : ApiController
+    {
+        // GET
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                Invoice i = new Invoice();
+                return Ok(i.GetAllInvoices());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // Post
+        public IHttpActionResult Post([FromBody] Invoice invoice)
+        {
+            try
+            {
+                Invoice newInvoice = invoice.PostInvoice();
+                return Ok(newInvoice);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // Delete
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                Invoice invoice = new Invoice();
+                int res = invoice.DeleteInvoice(id);
+                if(res == 1)
+                {
+                    return Ok();
+                }
+                return Content(HttpStatusCode.NotFound, $"invoice with id={id} was not found!!!");
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public IHttpActionResult Put(int id, [FromBody] Invoice invoice)
+        {
+            try
+            {
+                Invoice newInvoice = invoice.PutInvoice(id);
+                return Created(new Uri(Request.RequestUri.AbsoluteUri + newInvoice.Id), newInvoice);
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+    }
+}
